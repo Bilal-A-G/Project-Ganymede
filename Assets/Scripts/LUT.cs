22cb78e511983b4193c1671afee0ed40ad733927
unknown <bilalabdulg@gmail.com>
2023-02-05 11:53:47 -0500

    Removed everyone else's stuff

---
+++ Assets/Scripts/LUT.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LUT : MonoBehaviour
+{
+    [SerializeField] private Material lutMaterial;
+    public int lutType;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        lutMaterial.SetInt("_LUTType", lutType);
+        Graphics.Blit(src, dest, lutMaterial);
+    }
+}
